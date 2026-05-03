5374d1b4d193653191d366d215a45e884dc15f1c
Joshua Francis <jnf333@gmail.com>
2026-05-03 15:48:06 -0500

    Updated contact information

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -10,8 +10,8 @@
     <h2>Contact Information & Office Hours</h2>
 
     <p>Address: 1106 Highway 62 East, Suite 13 Mountain Home, AR 72653</p>
-    <p>Phone: <a href="tel:870-232-1911">870.232.1911</a></p>
-    <p>Email: <a href="mailto:vikki@francismassagemh.com">vikki@francismassagemh.com</a></p>
+    <p>Phone: <a href="tel:870-321-2564">870.321.2564</a></p>
+    <p>Email: <a href="mailto:vikki@ozarkmountainmassage.com">vikki@ozarkmountainmassage.com</a></p>
     <p>Office Hours: Monday-Wednesday, 8:30AM-3:00PM</p>
     <p>For questions, support, or more information, please do not hesitate to contact the administration office. We look forward to partnering with you on your educational journey.</p>
 </div>
